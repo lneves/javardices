d84cc32072c6f55d9ef83ccafb952389e6dca5b5
Luis Neves <luis.neves@co.sapo.pt>
2013-10-22 17:15:28 +0100

    allow other redirect codes

--- org.caudexorigo.http.netty3/src/main/java/org/caudexorigo/http/netty/RedirectAction.java
+++ org.caudexorigo.http.netty3/src/main/java/org/caudexorigo/http/netty/RedirectAction.java
@@ -1,6 +1,5 @@
 package org.caudexorigo.http.netty;
 
-import org.caudexorigo.http.netty.HttpAction;
 import org.jboss.netty.channel.ChannelHandlerContext;
 import org.jboss.netty.handler.codec.http.HttpHeaders;
 import org.jboss.netty.handler.codec.http.HttpRequest;
@@ -11,16 +10,30 @@ public class RedirectAction extends HttpAction
 {
 	private final String path;
 
+	private final HttpResponseStatus default_status;
+
 	public RedirectAction(String path)
+	{
+		this(path, HttpResponseStatus.FOUND);
+	}
+
+	public RedirectAction(String path, HttpResponseStatus status)
 	{
 		super();
 		this.path = path;
+
+		if (status.getCode() < 300 || status.getCode() > 310)
+		{
+			throw new IllegalArgumentException("Invalid status for redirect handler");
+		}
+
+		this.default_status = status;
 	}
 
 	@Override
 	public void service(ChannelHandlerContext ctx, HttpRequest req, HttpResponse res)
 	{
-		res.setStatus(HttpResponseStatus.FOUND);
+		res.setStatus(this.default_status);
 		res.addHeader(HttpHeaders.Names.LOCATION, path);
 	}
 }
