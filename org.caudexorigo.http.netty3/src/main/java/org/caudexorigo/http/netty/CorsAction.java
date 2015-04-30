edf3e3aebecd197fd08d9aeb536603be2343ac7f
Luis Neves <luis.neves@gmail.com>
2015-04-30 11:25:52 +0100

    add CorsAction

---
+++ org.caudexorigo.http.netty3/src/main/java/org/caudexorigo/http/netty/CorsAction.java
@@ -0,0 +1,19 @@
+package org.caudexorigo.http.netty;
+
+import org.jboss.netty.channel.ChannelHandlerContext;
+import org.jboss.netty.handler.codec.http.HttpHeaders;
+import org.jboss.netty.handler.codec.http.HttpRequest;
+import org.jboss.netty.handler.codec.http.HttpResponse;
+import org.jboss.netty.handler.codec.http.HttpResponseStatus;
+
+
+public class CorsAction extends HttpAction
+{
+	@Override
+	public void service(ChannelHandlerContext ctx, HttpRequest request, HttpResponse response)
+	{
+		response.headers().add(HttpHeaders.Names.ACCESS_CONTROL_ALLOW_ORIGIN, "*");
+		response.headers().add(HttpHeaders.Names.ALLOW, "GET,HEAD,POST,OPTIONS");
+		response.setStatus(HttpResponseStatus.OK);
+	}
+}
